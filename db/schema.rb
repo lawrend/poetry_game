9daf945928e66e5f887b0308badb019117117356
lawrend <douglaslawrence314@gmail.com>
2016-10-05 17:41:32 -0700

    migrated poem and round resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002110220) do
+ActiveRecord::Schema.define(version: 20161006003950) do
+
+  create_table "poems", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "line_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rounds", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
