5994ad7213990686c5d457043d9a4b7023e11ff4
lawrend <douglas_lawrence@mac.com>
2017-03-29 20:03:46 -0700

    added status to poem

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,5 +1,12 @@
 class Poem < ActiveRecord::Base
   belongs_to :user, inverse_of: :poems
   belongs_to :round
-  has_many :lines 
+  
+  enum staus: [:private, :participants, :public]
+  after_initialize :set_default_staus, :if => :new_record?
+
+  def set_default_status
+    self.status ||= :private
+  end
+  
 end
